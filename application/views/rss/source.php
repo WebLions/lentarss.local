bd43005cf5b40d8395580be0c117a6f40d1158eb
ArgeSwarge <argecata@mail.ru>
2016-02-09 21:43:04 +0200

    фикс

--- application/views/rss/source.php
+++ application/views/rss/source.php
@@ -7,7 +7,7 @@
 
                 <ol class="breadcrumb">
                     <li><i class="fa fa-home"></i><a href="/user/profile">Административная панель</a></li>
-                    <li><i class="fa fa-laptop"></i><a href="/rss/source">Источники</a></li>
+                    <li><i class="fa fa-laptop"></i>Источники</li>
                 </ol>
             </div>
         </div>
@@ -17,8 +17,7 @@
                 <form action="?" method="get">
                     <table width = "100%">
                         <tr>
-                            <td style = "width: 5%"><a class="btn btn-success" href="rss/add_source" title="Добавить источник">Добавить источник</a></td>
-                            <td style = "width: 5%"><a class="btn btn-success" href="rss/check" title="Проверить источник">Проверить источник</a></td>
+                            <td style = "width: 5%"><a class="btn btn-success" href="/rss/add_source_item" title="Добавить источник">Добавить источник</a></td>
                             <td style = "width: 75%"><input type="text" name="tag" class="form-control" id="search" placeholder="Поиск по источникам"></td>
                             <td style = "width: 5%"><button class="btn btn-primary" href="" title="Найти ">Найти</button></td>
                         </tr>
